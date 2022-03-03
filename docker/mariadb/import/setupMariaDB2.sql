635e762b92990b0123ceacb932a25576b27f86ef
Jan <jan.hanack@gmail.com>
2022-03-03 14:56:10 +0100

    Add MariaDB

---
+++ docker/mariadb/import/setupMariaDB2.sql
@@ -0,0 +1,11 @@
+CREATE TABLE schema_b(
+    name varchar(30) NOT NULL,
+    sales NUMERIC NOT NULL,
+    region varchar(30) NOT NULL,
+    PRIMARY KEY (name, region)
+);
+
+INSERT INTO schema_b VALUES ('martha', 100, 'america');
+INSERT INTO schema_b VALUES ('bertha', 1200, 'europe');
+INSERT INTO schema_b VALUES ('moritz', 850.3, 'europe');
+INSERT INTO schema_b VALUES ('timmothy', 2001, 'asia');
